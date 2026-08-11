0e4f57d8b4803559da8701e27034c1619e62a535
pagururu-code <pagururu@gmail.com>
2026-08-11 20:48:54 +0900

    Wire subitem source picker

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import TripFlowApp from '@/components/TripFlow/TripFlowApp';
 import TimelineDisplayEnhancements from '@/components/TripFlow/TimelineDisplayEnhancements';
 import TimelineUxRefinements from '@/components/TripFlow/TimelineUxRefinements';
+import SubitemSourcePicker from '@/components/TripFlow/SubitemSourcePicker';
 
 export default function Page() {
-  return <><TripFlowApp /><TimelineDisplayEnhancements /><TimelineUxRefinements /></>;
+  return <><TripFlowApp /><TimelineDisplayEnhancements /><TimelineUxRefinements /><SubitemSourcePicker /></>;
 }
