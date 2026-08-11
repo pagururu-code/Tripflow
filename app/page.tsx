be05ea9e56453a8295edc3789947cc06780f05ce
pagururu-code <pagururu@gmail.com>
2026-08-11 20:43:57 +0900

    Load timeline UX refinements

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import TripFlowApp from '@/components/TripFlow/TripFlowApp';
 import TimelineDisplayEnhancements from '@/components/TripFlow/TimelineDisplayEnhancements';
+import TimelineUxRefinements from '@/components/TripFlow/TimelineUxRefinements';
 
 export default function Page() {
-  return <><TripFlowApp /><TimelineDisplayEnhancements /></>;
+  return <><TripFlowApp /><TimelineDisplayEnhancements /><TimelineUxRefinements /></>;
 }
